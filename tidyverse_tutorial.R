12044ff2698824c247a1fbf034c7bcfa7087ec0b
Sarah C Thorngate <sct8690@library.northwestern.edu>
2024-03-19 17:11:27 -0500

    initial file creation

---
+++ tidyverse_tutorial.R
@@ -0,0 +1,9 @@
+#Tidyverse is a collection of R packages that provides a cohesive framework for cleaning, manipulatin, analyzing, and visualizing data. 
+
+#Load tidyverse. You could also load just the individual packages you'll be using.
+
+#install.packages("tidyverse")
+
+library(tidyverse)
+
+#read in a flatfile
