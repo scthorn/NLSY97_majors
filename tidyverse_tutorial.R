7db07b1d56f7e4842beffbcb81a8a342e1b887ea
Sarah C Thorngate <sct8690@library.northwestern.edu>
2024-03-28 17:21:38 -0500

    add more explanatory comments, redo dir structure

--- tidyverse_tutorial.R
+++ tidyverse_tutorial.R
@@ -6,13 +6,9 @@
 #This script walks you through the tidyverse by cleaning, reformatting, and exploring data from NLSY97, a large scale longitudinal survey conducted by the U.S. Bureau of Labor Statistics. 
 
 #--------------------------------------
-
-# Uncomment to set the working directory to the directory of the current script. Only works in RStudio.
-#try(setwd(dirname(rstudioapi::getActiveDocumentContext()$path)))
-
 # load tidyverse, a collection of R packages that provides a cohesive framework for cleaning, manipulating, analyzing, and visualizing data.
 
-#install.packages("tidyverse")
+#install.packages("tidyverse") #uncomment this line if you haven't installed tidyverse yet
 
 library(tidyverse)
 
@@ -20,15 +16,23 @@ library(tidyverse)
 
 # import data using readr, a package that provides a fast and friendly way to read rectangular data (like csv files) into R. Save it as a dataframe called df.
 
-df <- read_csv("data/input/tidytutorial.csv")
+#The file path I used here is a relative path. This means that the file is in a folder called 'data' that is in the same directory as this script.
+# If you're using RStudio, uncomment the line below to set the working directory to the directory of the current script.
+#Otherwise, you'll need to change the code to specify the full path to the file.
+
+#try(setwd(dirname(rstudioapi::getActiveDocumentContext()$path)))
+
+df <- read_csv("data/tidytutorial.csv") #read in the data
 
 # all sorts of ways to take a look at the data
 
-df 
+df #prints the entire dataframe to the console
 
-glimpse(df)
+glimpse(df) #prints a summary of the dataframe to the console
 
-summary(df)
+head(df) #prints the first few rows of the dataframe to the console
+
+summary(df) #prints summary statistics for each variable to the console
 
 
 #--------------MANIPULATING DATA WITH DPLYR------------------------
@@ -38,15 +42,15 @@ summary(df)
 # ------------- common dplyr verbs -------------------------------      
 
 # work with columns
-rename(df, yob = R0536402) 
-select(df, R0000100, R0536300, R0536402)
-relocate(df, R0536402)
-mutate(df, ageish2021 = 2021 - R0536402)
+rename(df, yob = R0536402) #rename the variable R0536402 to yob
+select(df, R0000100, R0536300, R0536402) #select only these three variables
+relocate(df, R0536402) #move the variable R0536402 to the first column
+mutate(df, ageish2021 = 2021 - R0536402) #create a new variable that calculates each R's approximate age in 2021
 
 # work with rows
-filter(df, R0536402 == 1980) 
-slice(df, 1:3)
-sample_n(df, 3) 
+filter(df, R0536402 == 1980) #only include cases where the year of birth is 1980
+slice(df, 1:3) #only include the first three cases
+sample_n(df, 3) #randomly select three cases
 
 # work with groups
 group_by(df, R0536402) #Usually you would do this to apply a function to each group (see below for example)
@@ -56,24 +60,26 @@ group_by(df, R0536402) #Usually you would do this to apply a function to each gr
 # you can use the 'pipe' to combine multiple actions. It takes the output of one function and uses it as the input to the next function.
 
 df %>%
-  rename(yob = R0536402) %>%
-  select(R0000100, R0536300, yob) %>%
-  relocate(yob) %>%
-  mutate(ageish2021 = 2021 - yob) %>%
-  filter(ageish2021 == 41)
+  rename(yob = R0536402) %>% #rename the variable R0536402 to yob
+  select(R0000100, R0536300, yob) %>% #select only these three variables
+  relocate(yob) %>% #move the variable yob to the first column
+  mutate(ageish2021 = 2021 - yob) %>% #create a new variable that calculates each R's approximate age in 2021
+  filter(ageish2021 == 41) #only include cases where the age in 2021 is 41
 
 df %>%
-  group_by(R0536402) %>%
+  group_by(R0536402) %>% #group by year of birth (R0536402)
   count() #this will count the number of Rs for each year of birth
 
 # The syntax as written above only prints to the console. If you want to save the output you need to assign it to a new dataframe.
 
-df_practice <- df %>%
+df_practice <- df %>% 
   rename(yob = R0536402) %>%
   select(R0000100, R0536300, yob) %>%
   relocate(yob) %>%
   mutate(ageish2021 = 2021 - yob) %>%
   filter(ageish2021 == 41)
+
+glimpse(df_practice)
 
 # ------------- working with the NLSY data -------------------------------      
 
@@ -123,11 +129,11 @@ df <- df |>
 #assign appropriate datatypes and recode from numbers to values
 df <- df |>
   relocate(yob:resmom_edu, starts_with("hhincome"), .after = hhnetworth_40) |> #we're moving variables just to lump datatypes together to make the next step simpler
-  mutate(across(ba_date:hhincome2021, ~ as.integer(.))) |> #convert to integer
-  mutate(across(sex:educ, ~ as.factor(.))) |> #convert to factors
+  mutate(across(ba_date:hhincome2021, ~ as.integer(.))) |> #convert these variables to integer
+  mutate(across(sex:educ, ~ as.factor(.))) |> #convert these variables to factors
   mutate(sex = fct_recode(sex, 
                           "male" = "1",
-                          "female" = "2")) |> #recode from numbers to values
+                          "female" = "2")) |> #recode from numbers to the values they represent
   mutate(race = fct_recode(race,
                            "black" = "1",
                            "hispanic" = "2",
@@ -170,31 +176,31 @@ df <- df |>
 #For demonstration purposes we'll use a subset of the data.
 
 demo <- df |>
-  select(ID, hhincome2017, hhincome2019, hhincome2021)
+  select(ID, hhincome2017, hhincome2019, hhincome2021) 
 
 demo #take a look. this data is in wide format
 
 #pivot_longer is a function that takes data in wide format and makes it longer.
 longer <- demo |>
-  pivot_longer(cols = starts_with("hhincome"),
-               names_to = "year", 
-               values_to = "hhincome")
+  pivot_longer(cols = starts_with("hhincome"), #specify the columns you want to make longer
+               names_to = "year", #name the new variable that will hold the column names
+               values_to = "hhincome") #name the new variable that will hold the values
 
 longer #take a look. this data is in long format
 
+#sidenote: you can use stringr to extract the year from the variable name 
 longer <- longer |>
-  mutate(year = str_sub(year, -4)) #sidenote: you can use stringr to extract the year from the variable name 
-
+  mutate(year = str_sub(year, -4)) #str_sub is a function that extracts a substring from a string. Here I'm saying to take the last four characters.
 #pivot_wider is a function that takes data in long format and spreads it out.
 
 back_to_wide <- longer |>
-  pivot_wider(names_from = year, 
-              values_from = hhincome)
+  pivot_wider(names_from = year,  #specify the variable whose values will become column names
+              values_from = hhincome) #specify the variable whose values will become the cell values
 
 back_to_wide #take a look. this data is back in wide format
 
 back_to_wide <- back_to_wide |>
-  rename_with(~str_c("hhincome", .x), '2017':'2021') #sidenote: you can use stringr to combine two strings for the variable name 
+  rename_with(~str_c("hhincome", .x), '2017':'2021') #str_c is a function that concatenates strings. Here I'm adding "hhincome" to the beginning of each variable name
 
 # ------------- exercises 2 -------------------------------      
 
@@ -204,81 +210,27 @@ back_to_wide <- back_to_wide |>
 #trickier
 
 #3. Rename the household net worth variables in df_networth to "at30", "at35", and "at40."
+#4. Create a new variable for the year each R was 30.
 
 
-#--------------CONSTRUCT A DATASET FOR ANALYSIS------------------------
-
-#Let's limit our population to Rs that participated in the most recent interview and create a simpler dataframe with only the variables we're interested in.
-
-df_simple <- df |>
-  filter(last_interview_round == "20") |> #filter to only include cases with data from the most recent round of interviews
-  select(ID:race, educ:ma_date, yob:resmom_edu, hhincome2021) #select variables of interest
-
-
-df_simple|>
-  summarize(across(everything(), ~ sum(is.na(.)))) #this gives a count of NA values for each variable
-
-#for today's purposes we'll just drop cases that are missing hhincome2021 or edu
-df_simple <- df_simple |>
-  filter(!is.na(hhincome2021) & !is.na(educ)) #filter to only include cases with non-missing values for hhincome2021 and educ
-
-#I think the reason we're seeing so many missing values for degree dates is because the respondent hasn't completed that degree. But let's check that logic. 
-df_simple |>
-  group_by(educ) |> #group by educational attainment
-  summarize(across(ba_date:ma_date, ~ sum(is.na(.)))) #count missing values for degree dates by educational attainment
-
-#That's not what I was hoping to see. Let's just get rid of those variables. We don't need them.
-df_simple <- df_simple |>
-  select(-ba_date, -prof_date, -phd_date, -ma_date) #the '-' tells R to drop the variables
-
-###consolidate parent education variables
-
-#We don't need four separate parent education variables. Let's consolidate them into a single variable indicating the highest level of education achieved by any parent.
-
-#first we have to make it ordinal.
-ranking <- c("less than HS" = 1, "HS" = 2, "Some college" = 3, "BA or more" = 4) #create a variable to hold the ranking for the levels of education
-
-df_simple <- df_simple |> 
-  mutate(across(biodad_edu:resmom_edu, ~ factor(., levels = names(ranking), ordered = TRUE))) #convert to factor with the levels we want
-
-#Now we can create a variable for the highest level of education achieved by any parent.
-df_simple <- df_simple |>
-  mutate(parent_edu = pmax(as.numeric(biodad_edu), as.numeric(biomom_edu), as.numeric(resdad_edu), as.numeric(resmom_edu), na.rm = TRUE)) |> #pmax returns the maximum value for each row
-  mutate(parent_edu = factor(parent_edu, levels = 1:4, labels = names(ranking), ordered = TRUE)) #convert to factor with the levels we want
-
-#Take a look to ensure we did what we meant to do.
-df_simple
-
-#Now we can drop the individual parent education variables and the cases where no parent education data is available.
-df_simple <- df_simple |>
-  select(-biodad_edu, -biomom_edu, -resdad_edu, -resmom_edu) |>
-  filter(!is.na(parent_edu))
-
-#We can save this dataset by writing to a csv. Note that I put my output data in a different directory than my input data.
-write_csv(df_simple, "data/output/df_simple.csv")
-
-# ------------- exercises 3 -------------------------------      
-
-#1. Using the 'df_networth' dataframe,  you created in the second set of exercises, create a new variable for the year R was 30.
-#2. Save the df_networth dataframe as a csv in the output directory. 
-
-#trickier
-#3. Create a dataframe with these variables: ID, the most recent networth value for each R, and yob. 
-#4. Add a variable for the year R was the age represented by their most recent networth value.
 
 #--------------DESCRIPTIVE STATS WITH DPLYR------------------------
+
+#I've created a simplified dataframe to demonstrate some basic descriptive statistics and exploratory visualization.
+
+df_simple <- read.csv("data/df_simple.csv") #read in the data, remember to change the file path if you need to
 
 # ------------- univariate frequencies and summary stats -------------------------------      
 
 #categorical variable
 df_simple |>
-  group_by(parent_edu) |>  
-  count()
+  group_by(parent_edu) |>  #group by a variable
+  count() #count the number of cases in each group
 
 #continuous variable
-df_simple |>
+df_simple |> 
   summarize(mean_income = mean(hhincome2021, na.rm = TRUE), #the na.rm argument tells R to ignore missing values
-            median_income = median(hhincome2021, na.rm = TRUE),
+            median_income = median(hhincome2021, na.rm = TRUE), 
             sd_income = sd(hhincome2021, na.rm = TRUE),
             min_income = min(hhincome2021, na.rm = TRUE),
             max_income = max(hhincome2021, na.rm = TRUE))
@@ -286,19 +238,19 @@ df_simple |>
 # ------------- bivariate frequencies (crosstabs) and summary stats -------------------------------      
 
 df_simple |>
-  group_by(parent_edu, educ) |>
-  count() |>
-  pivot_wider(names_from = educ, values_from = n, values_fill = 0)
+  group_by(parent_edu, educ) |> #group by two variables
+  count() |> #count the number of cases for each combination of the two variables
+  pivot_wider(names_from = educ, values_from = n, values_fill = 0) #spread the data out to display as crosstabs
 
 df_simple |>
-  group_by(parent_edu) |>
+  group_by(parent_edu) |> #group by parents education, then calculate summary stats per group
   summarize(mean_income = mean(hhincome2021, na.rm = TRUE),
             median_income = median(hhincome2021, na.rm = TRUE),
             sd_income = sd(hhincome2021, na.rm = TRUE),
             min_income = min(hhincome2021, na.rm = TRUE),
             max_income = max(hhincome2021, na.rm = TRUE))
 
-#--------------EXPLORATORY VISUALIZATION WITH GGPLOT-----------------------
+#--------------EXPLORATORY VISUALIZATION WITH GGPLOT (if we have time)-----------------------
 
 #ggplot2 is a package that implements the grammar of graphics. It's a powerful tool for creating visualizations.
 
@@ -306,9 +258,11 @@ df_simple |>
 
 # ------------- univariate   -------------------------------      
 
+#categorical variable
 ggplot(df_simple, aes(x = parent_edu)) + #specify the dataset and the variable you want to plot
   geom_bar()  #add a layer for the plot type
 
+#continuous variable
 ggplot(df_simple, aes(x = hhincome2021)) + 
   geom_histogram()
 
@@ -339,9 +293,9 @@ ggplot(df_simple, aes(x = hhincome2021)) +
 
 #Complete these questions using df_networth:
 
-#1. What is the distribution of this sample by race? Run summary stats and visualize this distribution.
-#2. How is the urbanrural variable distributed across racial categories in this sample? Run summary stats and visualize this relationship.
-#3. How does household networth vary by race in this sample? Run summary stats and visualize this relationship. 
+#1. What is the distribution of this sample by race? Run summary stats. If you have time, visualize this relationship with ggplot.
+#2. How is the urbanrural variable distributed across racial categories in this sample? Run summary stats. If you have time, visualize this relationship with ggplot.
+#3. How does household networth vary by race in this sample? Run summary stats. If you have time, visualize this relationship with ggplot.
 
 #trickier
 #4. Merge df_networth with df_simple. You will need to use join, a dplyr verb we didn't try. Ask google/chatgpt about it, or type '?join" into your console to read the docs.
